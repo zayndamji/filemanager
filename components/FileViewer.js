e670bb6801c1b0676455964ffc648eebe30012d6
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-07-03 20:26:57 -0700

    moved audio viewing into seperate component

--- components/FileViewer.js
+++ components/FileViewer.js
@@ -1,12 +1,14 @@
+import AudioFile from "./AudioFile";
+
 export default function FileViewer({ file }) {
   return (
     <div>
       {file.type && (
         <div className='mb-4'>
           {file.type.startsWith('audio') ? (
-            <audio controls src={URL.createObjectURL(file)} style={{width: "96%", margin: "2%"}}>
-              Your browser does not support the audio element.
-            </audio>
+            <AudioFile file={file} />
+          ) : file.type.startsWith('text') ? (
+            <pre>{file.type}</pre>
           ) : (
             <p>Unsupported file type: {file.type}</p>
           )}
