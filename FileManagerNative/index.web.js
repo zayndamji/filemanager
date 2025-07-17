f4d7e0b9cbdac0d792e5b67d375c7fc65107bd18
zayndamji <86502397+zayndamji@users.noreply.github.com>
2025-07-17 13:55:29 -0700

    Reapply "test changes"
    This reverts commit fd07d5e201c3bb3f4a53b3a3b6b48c325b1c4227.

--- FileManagerNative/index.web.js
+++ FileManagerNative/index.web.js
@@ -1,3 +1,9 @@
+// Polyfill Buffer and fromBuffer for web before anything else
+import { Buffer } from 'buffer';
+globalThis.Buffer = Buffer;
+if (!globalThis.Buffer.fromBuffer) {
+  globalThis.Buffer.fromBuffer = (buf) => globalThis.Buffer.from(buf);
+}
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
